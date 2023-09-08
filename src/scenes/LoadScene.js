8045b128708da5c915d1049a568c3f44f6fbf649
wkdgmr <wkdoov@gmail.com>
2023-09-08 17:46:41 -0400

    fixing LoadScene.js

--- src/scenes/LoadScene.js
+++ src/scenes/LoadScene.js
@@ -10,28 +10,28 @@ class LoadScene extends Phaser.Scene {
     const baseUrl = process.env.REACT_APP_BASE_URL;
 
     // background
-    this.load.image('background', `${baseUrl}assets/background.png`);
+    this.load.image('background', `${baseUrl}/assets/background.png`);
 
     // audio assets
     // jump
-    this.load.audio('jump', [`${baseUrl}assets/jump.ogg`, `${baseUrl}assets/jump.mp3`]);
+    this.load.audio('jump', [`${baseUrl}/assets/jump.ogg`, `${baseUrl}/assets/jump.mp3`]);
     // take coin
-    this.load.audio('coin', [`${baseUrl}assets/coin.ogg`, `${baseUrl}assets/coin.mp3`]);
+    this.load.audio('coin', [`${baseUrl}/assets/coin.ogg`, `${baseUrl}/assets/coin.mp3`]);
     // player dies
-    this.load.audio('dead', [`${baseUrl}assets/dead.ogg`, `${baseUrl}assets/dead.mp3`]);
+    this.load.audio('dead', [`${baseUrl}/assets/dead.ogg`, `${baseUrl}/assets/dead.mp3`]);
 
     // player assets
-    this.load.image('player', `${baseUrl}assets/player.png`);
+    this.load.image('player', `${baseUrl}/assets/player.png`);
 
     // monster assets
-    this.load.image('enemy', `${baseUrl}assets/enemy.png`);
+    this.load.image('enemy', `${baseUrl}/assets/enemy.png`);
 
     // interactive object assets
-    this.load.image('coin', `${baseUrl}assets/coin.png`);
+    this.load.image('coin', `${baseUrl}/assets/coin.png`);
 
     // level assets
-    this.load.image('wallV', `${baseUrl}assets/wallVertical.png`);
-    this.load.image('wallH', `${baseUrl}assets/wallHorizontal.png`);
+    this.load.image('wallV', `${baseUrl}/assets/wallVertical.png`);
+    this.load.image('wallH', `${baseUrl}/assets/wallHorizontal.png`);
 
     // Display a loading label
     const loadLabel = this.add.text(250, 170, 'loading', {
